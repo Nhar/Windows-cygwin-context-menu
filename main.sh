e4de5ae1aaf7f2541326f423cbb0ab00ae622df4
KKKKK <kosiarzksrz@gmail.com>
2019-09-29 21:04:50 +0200

    Rev. 2d Refractoring

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 #load files
 source "./scripts/menu.sh"
 source "./scripts/filesMan.sh"
+source "properties.sh"
 
 #main
 #input data
--- properties.sh
+++ properties.sh
@@ -1,2 +1,4 @@
-#Properties
+#Properties 
+#Use tripple backslashes
 CYGWIN_PATH="C:\\\cygwin64\\\bin\\\mintty.exe"
+CMD_PATH="C:\\\Windows\\\System32\\\cmd.exe"
